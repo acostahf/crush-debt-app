397f54429a47be33252385ea9536f2ee72405933
Fabian <acostahf4@gmail.com>
2024-02-28 12:23:38 -0600

    simple welcome screen

--- src/app/onboarding/welcome.tsx
+++ src/app/onboarding/welcome.tsx
@@ -1,11 +1,15 @@
 import NextStepBtn from "@/components/buttons/nextStepBtn";
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
 
 const Welcome = () => {
 	return (
-		<View>
-			<Text>welcome</Text>
+		<View className="flex-1 items-center justify-center p-4 bg-white">
+			<Text className="text-4xl font-bold text-center mb-6">Welcome!</Text>
+			<Text className="text-lg text-center mb-8">
+				We're excited to have you onboard. You'll be guided through a few
+				steps to complete your onboarding process.
+			</Text>
 			<NextStepBtn />
 		</View>
 	);

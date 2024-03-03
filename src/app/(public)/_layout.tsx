0f0610611afe07389ff68da1a4049cc10a6bb2a9
Fabian <acostahf4@gmail.com>
2024-03-03 16:09:58 -0600

    updated sturcture to support protected routes and replace now works properly

---
+++ src/app/(public)/_layout.tsx
@@ -0,0 +1,6 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+export default () => {
+	return <Stack />;
+};
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,47 +1,6 @@
-import {
-	ContentContainer,
-	HeaderContainer,
-	PageContainer,
-} from "@/components/ui/Containers";
-import { Link, Redirect, Stack } from "expo-router";
-import React, { useEffect, useState } from "react";
-import { onAuthStateChanged, User } from "firebase/auth";
-import { auth } from "@/services/firebaseConfig";
-import Dashboard from "./(tabs)/dashboard";
+import { Redirect } from "expo-router";
+import React from "react";
 
 export default function Page() {
-	const [user, setUser] = useState<User | null>(null);
-
-	useEffect(() => {
-		onAuthStateChanged(auth, (user) => {
-			setUser(user);
-		});
-	}, []);
-
-	return <>{user ? <AuthenticatedScreen /> : <HomeScreen />}</>;
+	return <Redirect href="welcome" />;
 }
-
-export const HomeScreen = () => {
-	return (
-		<PageContainer>
-			<Stack.Screen
-				options={{
-					headerRight: () => <Link href={"/login"}>Login</Link>,
-				}}
-			/>
-			<ContentContainer className="px-9">
-				<HeaderContainer>
-					<Link href={"/onboarding"}>Create Account</Link>
-				</HeaderContainer>
-			</ContentContainer>
-		</PageContainer>
-	);
-};
-
-export const AuthenticatedScreen = () => {
-	return (
-		<PageContainer>
-			<Dashboard />
-		</PageContainer>
-	);
-};
